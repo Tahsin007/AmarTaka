2579ef3900590daa5a84014e89164ac7437f29c5
USER <tahsinahmed.iit@gmail.com>
2025-08-02 14:52:13 +0600

    .env file initialized

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AppConstants{
+  static const String appName = 'AmarTaka';
+  static const String version = '1.0.0';
+  static String? apiBaseUrl = dotenv.env['API_URL'];
+}
